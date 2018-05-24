a7178a8202a7003cb5eee1715f8d64988fa0fb5e
zorkow <volker.sorge@gmail.com>
2018-05-24 14:50:15 -0700

    Rudimentary navigation.

--- annotations/chromify.js
+++ annotations/chromify.js
@@ -1,3 +1,5 @@
+const chromify = {};
+
 let id = 0;
 
 // rendered CommonHTML with SRE annotation
@@ -5,25 +7,25 @@ let id = 0;
 // -speech => aria-label
 // non-empty text-node => div.inline around it with aria-hidden=true
 
-let ariaowners = function(node) {
+chromify.ariaowners = function(node) {
     if (node.hasAttribute('data-semantic-children')) {
         node.setAttribute('aria-owns', node.getAttribute('data-semantic-children').replace(/,/g,' '));
     }
 }
 
-let makeid = function(node) {
+chromify.makeid = function(node) {
     if (node.hasAttribute('data-semantic-id')) {
         node.id = node.getAttribute('data-semantic-id');
     }
 }
 
-let speechers = function(node) {
+chromify.speechers = function(node) {
     if (node.hasAttribute('data-semantic-speech')) {
         node.setAttribute('aria-label', node.getAttribute('data-semantic-speech'));
     }
 }
 
-let rewrite = function(node) {
+chromify.rewrite = function(node) {
     if (node.nodeType === 3) {
         // if (node.textContent.trim() === '') return;
         let div = document.createElement('div');
@@ -43,9 +45,43 @@ let rewrite = function(node) {
         rewrite(child);
     }
 }
-rewrite(document.querySelector('span'))
 
+/**
+ * Key codes.
+ * @enum {number}
+ */
+chromify.KeyCode = {
+  ENTER: 13,
+  ESC: 27,
+  SPACE: 32,
+  PAGE_UP: 33,    // also NUM_NORTH_EAST
+  PAGE_DOWN: 34,  // also NUM_SOUTH_EAST
+  END: 35,        // also NUM_SOUTH_WEST
+  HOME: 36,       // also NUM_NORTH_WEST
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40,
+  TAB: 9
+};
 
-const attachNavigator = function(node){
-    node.setAttribute('tabindex', '0');
-}
+chromify.attachNavigator = function(node) {
+  node.setAttribute('tabindex', '0');
+  node.setAttribute('role', 'group');
+  document.addEventListener('keydown',function(event){
+    switch(event.keyCode){
+    case 37: //left
+    case 38: //up
+    case 39: //right
+    case 40: //down
+      node.setAttribute('aria-activedescendant', node.getAttribute('aria-owns'));
+      break;
+    }
+  });
+};
+
+chromify.attach = function() {
+  let node = document.querySelector('span');
+  chromify.rewrite(node);
+  chromify.attachNavigator(node);
+};
